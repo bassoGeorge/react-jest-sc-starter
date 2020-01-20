5465e5c95d7ca2cde13e449a78a021465baad509
Anish George <anishgeo@thoughtworks.com>
2020-01-20 14:11:44 +0530

    Install React

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log("Hello, World!")
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {App} from './App'
+
+const wrapper = document.getElementById("app");
+ReactDOM.render(<App/>, wrapper)

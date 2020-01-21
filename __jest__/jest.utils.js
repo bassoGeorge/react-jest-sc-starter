8bc94061e24ec6128c08e839d4d8bf3714d3f821
Anish George <anishgeo@thoughtworks.com>
2020-01-21 12:53:42 +0530

    Setup unit and integration configurations for testing

---
+++ __jest__/jest.utils.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+const commonSetup = {
+	rootDir           : path.resolve(__dirname, "../"),
+	setupFilesAfterEnv: ["<rootDir>/__jest__/jest.test-setup.js"]
+}
+
+const allProjects = {
+	unit       : {
+		...commonSetup,
+		displayName: "Unit Tests",
+		testRegex  : "src/.*\\.spec\\.jsx?$"
+	},
+	integration: {
+		...commonSetup,
+		displayName: "Integration Tests",
+		testRegex  : "(integrationTests|src)/.*\\.int\\.jsx?$"
+	}
+}
+
+function buildConfig(projects) {
+	return {
+		projects: projects.map(function (p) { return allProjects[p]; })
+	}
+}
+
+module.exports = {
+	buildConfig,
+	commonSetup
+}

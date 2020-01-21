9425182145c18b96368d96b8bf2a35bcbd44ae1d
Anish George <anishgeo@thoughtworks.com>
2020-01-21 13:25:08 +0530

    Add styled-components library

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import React from 'react'
 export function App() {
 	return (
 		<div>
-			<h1>Hello World!</h1>
+			<h1 css={`
+				color: green;
+				font-size: 4rem;
+			`}>Hello World!</h1>
 		</div>
 	)
 }

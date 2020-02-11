63048e526ca8b6c9da35dc8ca31eec53ea6b9c9f
Anish George <anishgeo@thoughtworks.com>
2020-02-11 12:48:34 +0530

    Add file and style mocks

--- .jest/jest.utils.js
+++ .jest/jest.utils.js
@@ -2,7 +2,12 @@ const path = require("path");
 
 const commonSetup = {
   rootDir: path.resolve(__dirname, "../"),
-  setupFilesAfterEnv: ["<rootDir>/.jest/jest.test-setup.js"]
+  setupFilesAfterEnv: ["<rootDir>/.jest/jest.test-setup.js"],
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/.jest/__mocks__/fileMock.js",
+    "\\.(css|scss)$": "<rootDir>/.jest/__mocks__/styleMock.js"
+  }
 };
 
 const allProjects = {
